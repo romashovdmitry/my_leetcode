332bfd4999408524c7a05732969a1a1a2a772b5a
romashovdmitry <romashov.dmitry.py@gmail.com>
2026-07-21 11:58:21 -0700

    upd

--- deleteme.py
+++ deleteme.py
@@ -7,17 +7,17 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
 
         word_set: set = set(wordDict)
-        boolean_list: List = [False for elem in range(len(s)+1)]        
+        boolean_list = [False for _ in range(len(s)+1)]
         boolean_list[0] = True
-        print_and_sleep(f'boolean list > {boolean_list}')
-        for i in range(len(s) + 1):
-            print_and_sleep(f's[i:] -> {s[i:]}')
-            for j in range(i, len(s) + 1):
-                print_and_sleep(f'under string -> {s[i:j]}')
-                if s[i] and s[i:j] in word_set:
-                    boolean_list[i+1] = True
+
+        for i in range(1, len(s) + 1):
+
+            for j in range(i):
+
+                if boolean_list[j] and s[j:i] in word_set:
+                    boolean_list[i] = True
                     break
-        print('come here')
+
         return boolean_list[-1]
 
 s = "leetcode"
